a34674f17ac6c7da7141e0feebf9ffbe02cc6126
midhunsankar23 <96018728+midhunsankar23@users.noreply.github.com>
2021-12-19 19:19:28 +0530

    Added list comprehension

--- blockchain.py
+++ blockchain.py
@@ -40,8 +40,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 def mine_block():
     last_block = blockchain[-1]
     hashed_block = ''
-    for key in last_block:
-        hashed_block = hashed_block + str(last_block[key])
+    hashed_block = '-'.join([str(last_block[key]) for key in last_block])
     print(hashed_block)
     block = {
         'previous_hash': hashed_block,
