a3cee5e500d6c33c0b83acfac6d10f69eb90c0d6
midhunsankar23 <96018728+midhunsankar23@users.noreply.github.com>
2021-12-25 11:13:21 +0530

    Major update to blockchain.py
    - Imports hashlib as hl
    - Added Proof of work
    - Added valid_proof() and proof_of_work() functions
    - valid_proof() can verify the proof is valid // returns true or false
    - proof_of_work() calculates the valid proof using a while loop and valid_proof() // runs the loop until a valid hash and proof is got
    - Updated the verify_chain() function
    - Added a proof validation for each blocks in verify_chain() function
    - verify_chain() only passes block['transactions'][:-1], block['previous_hash'], block['proof'] // block['transactions'][:-1] means it wont pass the last transaction which is not included in calculation of proof
    - Updated the mine_block() function
    - mine_block() now calls the proof_of_work() to fetch the valid proof and adds it to the block which is to be mined

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,5 @@
 import functools
-import hashlib
+import hashlib as hl
 import json
 
 # The reward we give to miners (for creating a new block)
@@ -9,7 +9,8 @@ MINING_REWARD = 10
 genesis_block = {
     'previous_hash': '',
     'index': 0,
-    'transactions': []
+    'transactions': [],
+    'proof' : 100
 }
 # Initializing our (empty) blockchain list
 blockchain = [genesis_block]
@@ -27,8 +28,23 @@ def hash_block(block):
     Arguments:
         :block: The block that should be hashed.
     """
-    return hashlib.sha256(json.dumps(block).encode()).hexdigest()
+    return hl.sha256(json.dumps(block).encode()).hexdigest()
 
+
+def valid_proof(transaction, lasthash, proof):
+    guess = (str(transaction) + str(lasthash) + str(proof)).encode()
+    guess_hash = hl.sha256(guess).hexdigest()
+    print(guess_hash)
+    return guess_hash[0:2] == '00'
+
+
+def proof_of_work():
+    last_block = blockchain[-1]
+    last_hash = hash_block(last_block)
+    proof = 0
+    while not valid_proof(open_transactions, last_hash, proof):
+        proof += 1
+    return proof
 
 def get_balance(participant):
     """Calculate and return the balance for a participant.
@@ -101,7 +117,7 @@ def mine_block():
     last_block = blockchain[-1]
     # Hash the last block (=> to be able to compare it to the stored hash value)
     hashed_block = hash_block(last_block)
-    print(hashed_block)
+    proof = proof_of_work()
     # Miners should be rewarded, so let's create a reward transaction
     reward_transaction = {
         'sender': 'MINING',
@@ -115,7 +131,8 @@ def mine_block():
     block = {
         'previous_hash': hashed_block,
         'index': len(blockchain),
-        'transactions': copied_transactions
+        'transactions': copied_transactions,
+        'proof': proof
     }
     blockchain.append(block)
     return True
@@ -151,6 +168,8 @@ def verify_chain():
         if index == 0:
             continue
         if block['previous_hash'] != hash_block(blockchain[index - 1]):
+            return False
+        if not valid_proof(block['transactions'][:-1], block['previous_hash'], block['proof']):
             return False
     return True
 
